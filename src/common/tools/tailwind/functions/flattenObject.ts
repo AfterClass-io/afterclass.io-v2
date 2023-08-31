e971fe355cd8192de64a02c587553fb1e252d402
Zachery Ng <zachery.ng@gmail.com>
2023-08-31 12:04:37 +0800

    feat: theming system w/ font

---
+++ src/common/tools/tailwind/functions/flattenObject.ts
@@ -0,0 +1,44 @@
+import { type RecursiveKeyValuePair } from "tailwindcss/types/config";
+
+import { hexToRGB } from "./hexToRGB";
+
+type FlattenedObject = Record<string, string>;
+
+/**
+ * Flattens a nested object and returns a new object with flattened keys.
+ *
+ * @param {RecursiveKeyValuePair<string, string>} obj - The nested object to flatten.
+ * @param {Object} options - Optional configuration for object flattening.
+ * @param {string} [options.parentKey=''] - The prefix to use for the flattened keys.
+ * @param {string} [options.separator='-'] - The separator to use between flattened key segments.
+ * @param {(hex: string) => string} [options.convertHexToRgb] - Optional function to convert HEX colors to RGB format.
+ * @returns {Record<string, string>} An object containing flattened key-value pairs.
+ *
+ * @template RecursiveKeyValuePair - The type of the nested object to flatten.
+ */
+export const flattenObject = (
+  obj: RecursiveKeyValuePair<string, string>,
+  { parentKey = "", separator = "-", convertHexToRgb = true }
+): Record<string, string> => {
+  return Object.keys(obj).reduce((acc, key) => {
+    const fullKey = parentKey ? `${parentKey}${separator}${key}` : key;
+    const value = obj[key];
+
+    if (typeof value === "object" && value !== null) {
+      const flattened = flattenObject(value, { parentKey: fullKey, separator });
+      Object.keys(flattened).forEach((subKey) => {
+        const color = convertHexToRgb
+          ? hexToRGB(flattened[subKey])
+          : flattened[subKey];
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        acc[subKey] = subKey.endsWith("-DEFAULT") ? color : flattened[subKey];
+      });
+    } else {
+      const color = convertHexToRgb ? hexToRGB(value as string) : value;
+      acc[fullKey] = color as string;
+    }
+
+    return acc;
+  }, {} as FlattenedObject);
+};
---
+++ src/common/tools/tailwind/themes/appTheme.ts
@@ -0,0 +1,45 @@
+import rgb from "../functions/rgb";
+
+export const APP_THEMES = {
+  dark: "dark",
+  light: "light",
+};
+
+export type AppTheme = keyof typeof APP_THEMES;
+
+export const appTheme = {
+  colors: {
+    primary: {
+      default: rgb(`--app-primary-default`),
+      dark: rgb(`--app-primary-dark`),
+    },
+    secondary: {
+      default: rgb("--app-secondary-default"),
+    },
+    element: {
+      secondary: rgb("--app-element-secondary"),
+      tertiary: rgb("--app-element-tertiary"),
+    },
+    bg: {
+      base: rgb("--app-bg-base"),
+      alt: rgb("--app-bg-alt"),
+    },
+    surface: {
+      base: rgb("--app-surface-base"),
+      elevated: rgb("--app-surface-elevated"),
+    },
+    border: {
+      default: rgb("--app-border-default"),
+      elevated: rgb("--app-border-elevated"),
+    },
+    text: {
+      "on-primary": rgb("--app-text-on-primary"),
+      "on-secondary": rgb("--app-text-on-secondary"),
+      "on-tertiary": rgb("--app-text-on-tertiary"),
+      "em-high": rgb("--app-text-em-high"),
+      "em-mid": rgb("--app-text-em-mid"),
+      "em-low": rgb("--app-text-em-low"),
+      placeholder: rgb("--app-text-placeholder"),
+    },
+  },
+};
