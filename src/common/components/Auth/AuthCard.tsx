f11f87b0409ca34ada7f0276a4a0a868c4a50250
Jerome Wong <jeromewongjh@gmail.com>
2023-09-10 23:04:50 +0800

    refactor: make AuthCard polymorphic

--- src/common/components/Auth/AuthCard.tsx
+++ src/common/components/Auth/AuthCard.tsx
@@ -1,18 +1,40 @@
-import { type ComponentPropsWithoutRef, forwardRef } from "react";
+import {
+  forwardRef,
+  type ComponentProps,
+  type ElementType,
+  type ReactNode,
+  type Ref,
+} from "react";
 
-export const AuthCard = forwardRef<
-  HTMLDivElement,
-  ComponentPropsWithoutRef<"div">
->(({ children, title = "Login" }, ref) => {
-  return (
-    <div
-      ref={ref}
-      className="mt-10 flex w-96 min-w-fit flex-col gap-y-5 rounded-lg bg-bg-alt px-4 py-5"
-    >
-      <div className="text-lg font-bold text-text-em-high">{title}</div>
-      <div>{children}</div>
-    </div>
-  );
-});
+type AuthCardProps<T extends ElementType = ElementType> = {
+  as?: T;
+  title?: string;
+  children?: ReactNode;
+};
+
+const defaultElement = "h1";
+
+type TitleProps<T extends ElementType> = AuthCardProps<T> &
+  Omit<ComponentProps<T>, keyof AuthCardProps<T>>;
+
+export const AuthCard = forwardRef(
+  <T extends ElementType = typeof defaultElement>(
+    { children, title = "Login", as, ...rest }: TitleProps<T>,
+    ref: Ref<HTMLDivElement>
+  ) => {
+    const Title = as || defaultElement;
+    return (
+      <div
+        ref={ref}
+        className="mt-10 flex w-96 min-w-fit flex-col gap-y-5 rounded-lg bg-bg-alt px-4 py-5"
+      >
+        <Title className="text-lg font-bold text-text-em-high" {...rest}>
+          {title}
+        </Title>
+        <div>{children}</div>
+      </div>
+    );
+  }
+);
 
 AuthCard.displayName = "AuthCard";
