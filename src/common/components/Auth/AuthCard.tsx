f38ca50344465daf38074135aa18d62ce3bc40bb
Jerome Wong <jeromewongjh@gmail.com>
2023-09-11 17:49:43 +0800

    refactor: remove forwardRef

--- src/common/components/Auth/AuthCard.tsx
+++ src/common/components/Auth/AuthCard.tsx
@@ -1,10 +1,4 @@
-import {
-  forwardRef,
-  type ComponentProps,
-  type ElementType,
-  type ReactNode,
-  type Ref,
-} from "react";
+import { type ComponentProps, type ElementType, type ReactNode } from "react";
 
 type AuthCardProps<T extends ElementType = ElementType> = {
   as?: T;
@@ -17,24 +11,21 @@ const defaultElement = "h1";
 type TitleProps<T extends ElementType> = AuthCardProps<T> &
   Omit<ComponentProps<T>, keyof AuthCardProps<T>>;
 
-export const AuthCard = forwardRef(
-  <T extends ElementType = typeof defaultElement>(
-    { children, title = "Login", as, ...rest }: TitleProps<T>,
-    ref: Ref<HTMLDivElement>
-  ) => {
-    const Title = as || defaultElement;
-    return (
-      <div
-        ref={ref}
-        className="mt-10 flex w-96 min-w-fit flex-col gap-y-5 rounded-lg bg-bg-alt px-4 py-5"
-      >
-        <Title className="text-lg font-bold text-text-em-high" {...rest}>
-          {title}
-        </Title>
-        <div>{children}</div>
-      </div>
-    );
-  }
-);
+export const AuthCard = <T extends ElementType = typeof defaultElement>({
+  children,
+  title = "Login",
+  as,
+  ...rest
+}: TitleProps<T>) => {
+  const Title = as || defaultElement;
+  return (
+    <div className="mt-10 flex w-96 min-w-fit flex-col gap-y-5 rounded-lg bg-bg-alt px-4 py-5">
+      <Title className="text-lg font-bold text-text-em-high" {...rest}>
+        {title}
+      </Title>
+      <div>{children}</div>
+    </div>
+  );
+};
 
 AuthCard.displayName = "AuthCard";
