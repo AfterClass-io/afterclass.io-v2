4c45bd09d26dfb77676eef19135afc57b64e0152
David Lin <13061926+DavidLHW@users.noreply.github.com>
2023-10-18 20:55:15 +0800

    update input component: wrap with Field

--- src/common/components/Input/Input.tsx
+++ src/common/components/Input/Input.tsx
@@ -1,8 +1,10 @@
 import { type ReactNode, type ComponentPropsWithoutRef } from "react";
 import { type InputVariants, inputTheme } from "./Input.theme";
+import { Field, FieldProps } from "@/common/components/Field";
 
 export type InputProps = Omit<ComponentPropsWithoutRef<"input">, "size"> &
-  InputVariants & {
+  InputVariants &
+  FieldProps & {
     leftContent?: ReactNode;
     rightContent?: ReactNode;
     wrapperProps?: ComponentPropsWithoutRef<"div">;
@@ -14,17 +16,22 @@ export const Input = ({
   leftContent,
   rightContent,
   wrapperProps,
+  label,
+  helperText,
+  isError = false,
   ...props
 }: InputProps) => {
   const { input: inputClasses, wrapper } = inputTheme({ className, size });
   return (
-    <div
-      {...wrapperProps}
-      className={wrapper({ className: wrapperProps?.className })}
-    >
-      {leftContent}
-      <input {...props} className={inputClasses({ className })} />
-      {rightContent}
-    </div>
+    <Field label={label} isError={isError} helperText={helperText}>
+      <div
+        {...wrapperProps}
+        className={wrapper({ className: wrapperProps?.className })}
+      >
+        {leftContent}
+        <input {...props} className={inputClasses({ className })} />
+        {rightContent}
+      </div>
+    </Field>
   );
 };
