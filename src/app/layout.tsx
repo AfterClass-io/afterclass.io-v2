fc199c38422eb8073510f9ebda86cfe4c745ff34
Zachery Ng <zachery.ng@gmail.com>
2023-12-02 15:21:42 +0800

    fix: fonts flashing on reload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { TRPCReactProvider } from "@/trpc/react";
 import { CoreLayout } from "@/common/components/CoreLayout";
 import ThemeProvider from "@/common/providers/ThemeProvider";
 import AuthProvider from "@/common/providers/AuthProvider";
+import { inter, poppins } from "@/common/fonts";
 
 export default function RootLayout({
   children,
@@ -13,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${inter.variable} ${poppins.variable}`}>
       <body>
         <ThemeProvider>
           <AuthProvider>
