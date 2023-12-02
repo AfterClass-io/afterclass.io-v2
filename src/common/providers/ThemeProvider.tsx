5c287e295cf45ce2fc3096f106c077f80c0f1571
Zachery Ng <zachery.ng@gmail.com>
2023-12-02 15:24:05 +0800

    chore: remove font import in themeprovider

--- src/common/providers/ThemeProvider.tsx
+++ src/common/providers/ThemeProvider.tsx
@@ -2,7 +2,6 @@
 
 import { ThemeProvider as NextThemeProvider } from "next-themes";
 import { APP_THEMES } from "@/common/tools/tailwind/themes/appTheme";
-import { inter, poppins } from "@/common/fonts";
 
 const NEXT_THEMES = Object.keys(APP_THEMES).map((key) => key);
 
@@ -13,13 +12,6 @@ export default function ThemeProvider({
 }) {
   return (
     <>
-      {/* This is required for fonts to work in Portal too */}
-      <style global jsx>{`
-        :root {
-          --font-inter: ${inter.style.fontFamily};
-          --font-poppins: ${poppins.style.fontFamily};
-        }
-      `}</style>
       <NextThemeProvider
         attribute="class"
         defaultTheme={APP_THEMES.dark}
