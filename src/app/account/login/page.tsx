f89202d42dcf3f887d2a47c5c8a2489438897d64
David Lin <13061926+DavidLHW@users.noreply.github.com>
2023-12-17 17:41:00 +0700

    feat: + login logic on login page

--- src/app/account/login/page.tsx
+++ src/app/account/login/page.tsx
@@ -6,9 +6,20 @@ import { useState } from "react";
 import { EnvelopeIcon } from "@/common/components/CustomIcon/EnvelopeIcon";
 import { LockIcon } from "@/common/components/CustomIcon/LockIcon";
 import { Button } from "@/common/components/Button";
+import { signIn } from "next-auth/react";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
   const [pwd, setPwd] = useState("");
+  const [formSubmittedLoading, setFormSubmittedLoading] = useState(false);
+  const signin = () => {
+    setFormSubmittedLoading(true);
+    signIn("credentials", {
+      email: email,
+      password: pwd,
+      callbackUrl: "/reviews",
+    });
+  };
 
   return (
     <>
@@ -18,19 +29,21 @@ export default function Login() {
             label={"School Email Address"}
             leftContent={<EnvelopeIcon size={24} />}
             placeholder="john.doe.2023@smu.edu.sg"
-            value={pwd}
-            onChange={(ev) => setPwd(ev.target.value)}
+            value={email}
+            onChange={(ev) => setEmail(ev.target.value)}
           />
           <Input
             label={"Password"}
             leftContent={<LockIcon size={24} />}
             placeholder="Enter password"
+            type="password"
             value={pwd}
             onChange={(ev) => setPwd(ev.target.value)}
+            onKeyDown={(ev) => ev.key === "Enter" && signin()}
           />
           <div className="flex w-full flex-col items-start gap-2 self-stretch pt-3">
-            <Button fullWidth onClick={() => console.log("login")}>
-              Login
+            <Button fullWidth onClick={signin} disabled={formSubmittedLoading}>
+              {formSubmittedLoading ? "Signing in..." : "Login"}
             </Button>
             <div className="flex items-center gap-1 self-stretch text-base">
               <span className="text-center font-semibold text-text-em-mid">
