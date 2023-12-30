a063e9759e8e4a0116394ca5104e63954cec000e
David Lin <13061926+DavidLHW@users.noreply.github.com>
2023-12-30 15:16:04 +0800

    chore: extract indeterminate state changes to be the responsibility of consumer

--- src/common/components/Checkbox/Checkbox.tsx
+++ src/common/components/Checkbox/Checkbox.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useId } from "react";
+import { forwardRef, useId } from "react";
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 import { CheckIcon, MinusIcon } from "@/common/components/CustomIcon";
 import {
@@ -8,65 +8,52 @@ import {
   type CheckboxVariants,
 } from "@/common/components/Checkbox";
 
+export type CheckedState = CheckboxPrimitive.CheckedState;
+
 export type CheckboxProps = CheckboxVariants &
   CheckboxPrimitive.CheckboxProps & {
-    label: string;
+    label?: string;
     indeterminate?: boolean;
   };
 
-export const Checkbox = ({
-  label,
-  className,
-  size = "md",
-  disabled = false,
-  indeterminate = false,
-  defaultChecked,
-  checked: initialCheckedState,
-  ...props
-}: CheckboxProps) => {
-  const initialState = () => {
-    if (initialCheckedState !== undefined) return initialCheckedState;
-    if (defaultChecked !== undefined) return defaultChecked;
-    return indeterminate ? "indeterminate" : false;
-  };
-  const [checked, setChecked] = useState(initialState());
-  const id = useId();
-  const {
-    wrapper,
-    checkboxRoot,
-    checkboxIndicator,
-    checkboxIndicatorIcon,
-    label: labelContainer,
-  } = checkboxTheme({
-    className,
-    size,
-    disabled,
-  });
+export const Checkbox = forwardRef<HTMLButtonElement, CheckboxProps>(
+  ({ label, className, size = "md", disabled = false, ...props }, ref) => {
+    const id = useId();
+    const {
+      wrapper,
+      checkboxRoot,
+      checkboxIndicator,
+      checkboxIndicatorIcon,
+      label: labelContainer,
+    } = checkboxTheme({
+      className,
+      size,
+      disabled,
+    });
 
-  return (
-    <div className={wrapper()}>
-      <CheckboxPrimitive.Root
-        id={id}
-        disabled={disabled}
-        checked={checked}
-        onCheckedChange={setChecked}
-        className={checkboxRoot()}
-        {...props}
-      >
-        <CheckboxPrimitive.Indicator className={checkboxIndicator()}>
-          {checked === "indeterminate" && (
-            <MinusIcon className={checkboxIndicatorIcon()} />
-          )}
-          {checked === true && (
-            <CheckIcon className={checkboxIndicatorIcon()} />
-          )}
-        </CheckboxPrimitive.Indicator>
-      </CheckboxPrimitive.Root>
-      {label && (
-        <label htmlFor={id} className={labelContainer()}>
-          {label}
-        </label>
-      )}
-    </div>
-  );
-};
+    return (
+      <div className={wrapper()}>
+        <CheckboxPrimitive.Root
+          id={id}
+          disabled={disabled}
+          className={checkboxRoot()}
+          {...props}
+          ref={ref}
+        >
+          <CheckboxPrimitive.Indicator className={checkboxIndicator()}>
+            {props.checked === "indeterminate" ? (
+              <MinusIcon className={checkboxIndicatorIcon()} />
+            ) : (
+              <CheckIcon className={checkboxIndicatorIcon()} />
+            )}
+          </CheckboxPrimitive.Indicator>
+        </CheckboxPrimitive.Root>
+        {label && (
+          <label htmlFor={id} className={labelContainer()}>
+            {label}
+          </label>
+        )}
+      </div>
+    );
+  },
+);
