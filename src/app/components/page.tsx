eb232260f5678c07cd5c9654710ecefe2ad1ec1b
David Lin <13061926+DavidLHW@users.noreply.github.com>
2023-12-31 18:20:58 +0800

    refactor: use children on dialog content

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -187,39 +187,29 @@ export default function Components() {
                   Set the dimensions for the layer.
                 </p>
               </div>
-              <div className="grid gap-2">
-                <div className="grid grid-cols-3 items-center gap-4">
-                  <label htmlFor="width">Width</label>
-                  <Input
-                    id="width"
-                    defaultValue="100%"
-                    className="col-span-2 h-8"
-                  />
-                </div>
-                <div className="grid grid-cols-3 items-center gap-4">
-                  <label htmlFor="maxWidth">Max. width</label>
-                  <Input
-                    id="maxWidth"
-                    defaultValue="300px"
-                    className="col-span-2 h-8"
-                  />
-                </div>
-                <div className="grid grid-cols-3 items-center gap-4">
-                  <label htmlFor="height">Height</label>
-                  <Input
-                    id="height"
-                    defaultValue="25px"
-                    className="col-span-2 h-8"
-                  />
-                </div>
-                <div className="grid grid-cols-3 items-center gap-4">
-                  <label htmlFor="maxHeight">Max. height</label>
-                  <Input
-                    id="maxHeight"
-                    defaultValue="none"
-                    className="col-span-2 h-8"
-                  />
-                </div>
+              <div className="grid grid-cols-3 items-center gap-4">
+                <label htmlFor="maxWidth">Max. width</label>
+                <Input
+                  id="maxWidth"
+                  defaultValue="300px"
+                  className="col-span-2 h-8"
+                />
+              </div>
+              <div className="grid grid-cols-3 items-center gap-4">
+                <label htmlFor="height">Height</label>
+                <Input
+                  id="height"
+                  defaultValue="25px"
+                  className="col-span-2 h-8"
+                />
+              </div>
+              <div className="grid grid-cols-3 items-center gap-4">
+                <label htmlFor="maxHeight">Max. height</label>
+                <Input
+                  id="maxHeight"
+                  defaultValue="none"
+                  className="col-span-2 h-8"
+                />
               </div>
             </div>
           </Popover.Content>
