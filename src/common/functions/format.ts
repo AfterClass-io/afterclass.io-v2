94a4243638f7967c9df28a367413bd0e0de67796
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-01-07 00:20:45 +0800

    chore: add format percentage fn

---
+++ src/common/functions/format.ts
@@ -0,0 +1,7 @@
+export const formatPercentage = (amount: number) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "percent",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(amount);
+};
