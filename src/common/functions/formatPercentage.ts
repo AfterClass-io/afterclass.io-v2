7aca4eabdbfa4fbf88c3444c67ba5fb6af4f6bfc
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-01-07 15:07:53 +0800

    refactor: rename function files to house only 1 fn

--- src/common/functions/formatPercentage.ts
+++ src/common/functions/formatPercentage.ts
@@ -1,7 +1,11 @@
-export const formatPercentage = (amount: number) => {
+export default function formatPercentage(
+  amount: number,
+  options: Intl.NumberFormatOptions | undefined = undefined,
+) {
   return new Intl.NumberFormat("en-US", {
     style: "percent",
     minimumFractionDigits: 0,
     maximumFractionDigits: 0,
+    ...options,
   }).format(amount);
-};
+}
