b2c66fb44b216e5ad943670fe534e6c51e7c00d1
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-01-21 17:02:45 +0800

    chore: add variant to lock cta overlay component

--- src/common/components/LockCtaOverlay/LockCtaOverlay.theme.ts
+++ src/common/components/LockCtaOverlay/LockCtaOverlay.theme.ts
@@ -19,26 +19,42 @@ export const lockCtaOverlayTheme = tv(
       ],
       wrapper: [
         "inline-flex",
+        "absolute",
+        "left-0",
+        "top-0",
         "items-center",
         "justify-center",
         "h-full",
         "w-full",
-        "gap-1",
         "text-text-em-low",
+        "z-10",
       ],
-      icon: ["h-6", "w-6"],
-      ctaTextContainer: [
-        "flex",
-        "items-center",
-        "gap-1",
-        "text-lg",
-        "font-medium",
-      ],
+      icon: [],
+      ctaTextContainer: ["flex", "items-center", "gap-1", "font-medium"],
     },
     variants: {
       size: {
-        md: {},
-        sm: {},
+        md: {
+          wrapper: ["gap-2"],
+          icon: ["h-6", "w-6"],
+          ctaTextContainer: ["text-lg"],
+        },
+        sm: {
+          wrapper: ["gap-[0.375rem]"],
+          icon: ["h-4", "w-4"],
+          ctaTextContainer: ["text-sm"],
+        },
+      },
+      variant: {
+        border: {
+          overlay: ["hidden"],
+          wrapper: [
+            "rounded-lg",
+            "border-2",
+            "border-solid",
+            "border-border-default",
+          ],
+        },
       },
     },
   },
--- src/common/components/LockCtaOverlay/LockCtaOverlay.tsx
+++ src/common/components/LockCtaOverlay/LockCtaOverlay.tsx
@@ -1,20 +1,40 @@
 import { Button } from "@/common/components/Button";
 import { LockIcon } from "@/common/components/CustomIcon/LockIcon";
-import { lockCtaOverlayTheme } from "./LockCtaOverlay.theme";
+import {
+  lockCtaOverlayTheme,
+  type LockCtaOverlayVariants,
+} from "./LockCtaOverlay.theme";
 
-export const LockCtaOverlay = () => {
-  const { wrapper, overlay, ctaTextContainer, icon } = lockCtaOverlayTheme();
+const ctaTextMap = {
+  rating: "to see rating",
+  review: "to see review",
+} as const;
+
+export type LockCtaOverlayProps = LockCtaOverlayVariants & {
+  ctaType?: keyof typeof ctaTextMap;
+};
+
+export const LockCtaOverlay = ({
+  variant,
+  size = "md",
+  ctaType = "rating",
+}: LockCtaOverlayProps) => {
+  const { wrapper, overlay, ctaTextContainer, icon } = lockCtaOverlayTheme({
+    variant,
+    size,
+  });
   return (
-    <div className={overlay()}>
+    <>
+      <div className={overlay()}></div>
       <div className={wrapper()}>
         <LockIcon className={icon()} />
         <div className={ctaTextContainer()}>
           <Button variant="link" as="a" href="/account/auth/login">
             Login
           </Button>
-          <span>to see rating</span>
+          <span>{ctaTextMap[ctaType]}</span>
         </div>
       </div>
-    </div>
+    </>
   );
 };
