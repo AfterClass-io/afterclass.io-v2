8610950e5065e7bdc6fc9a50589c39d2624d9ea9
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-03-04 18:17:29 +0800

    chore: remove default addons included in essentials

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,12 +2,7 @@ import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-themes",
-  ],
+  addons: ["@storybook/addon-essentials", "@storybook/addon-themes"],
   framework: {
     name: "@storybook/nextjs",
     options: {},
