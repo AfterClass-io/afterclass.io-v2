744ae95a3ee48a96454eb54a3924ebac8f97fdc2
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-03-31 04:41:12 +0800

    chore: breadcrumb storybook

--- src/common/components/Breadcrumb/Breadcrumb.stories.tsx
+++ src/common/components/Breadcrumb/Breadcrumb.stories.tsx
@@ -1,6 +1,8 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import { Breadcrumb } from "./Breadcrumb";
+import { DropdownMenu } from "../DropdownMenu";
+import { ChevronDownIcon } from "../CustomIcon";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
@@ -16,27 +18,44 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  render: () => {
-    return (
-      <Breadcrumb>
+  render: () => (
+    <Breadcrumb>
+      <Breadcrumb.List>
         <Breadcrumb.Item>
-          <Breadcrumb.Link href="#">Home</Breadcrumb.Link>
+          <Breadcrumb.Link href="/">Home</Breadcrumb.Link>
         </Breadcrumb.Item>
+        <Breadcrumb.Separator />
         <Breadcrumb.Item>
-          <Breadcrumb.Link href="#">Library</Breadcrumb.Link>
+          <Breadcrumb.Link href="/components">Components</Breadcrumb.Link>
         </Breadcrumb.Item>
-        <Breadcrumb.Item>
-          <Breadcrumb.Link href="#" aria-current="page">
-            Data
-          </Breadcrumb.Link>
-        </Breadcrumb.Item>
-      </Breadcrumb>
-    );
-  },
+      </Breadcrumb.List>
+    </Breadcrumb>
+  ),
 };
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-// export const DefaultLocked: Story = {
-//   ...Default,
-//   args: {},
-// };
+export const WithDropdown: Story = {
+  render: () => (
+    <Breadcrumb>
+      <Breadcrumb.List>
+        <Breadcrumb.Item>
+          <DropdownMenu>
+            <DropdownMenu.Trigger className="flex items-center gap-1">
+              All Schools
+              <ChevronDownIcon />
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content align="start">
+              <DropdownMenu.Item>SMU</DropdownMenu.Item>
+              <DropdownMenu.Item>NUS</DropdownMenu.Item>
+              <DropdownMenu.Item>NTU</DropdownMenu.Item>
+            </DropdownMenu.Content>
+          </DropdownMenu>
+        </Breadcrumb.Item>
+        <Breadcrumb.Separator />
+        <Breadcrumb.Item>
+          <Breadcrumb.Link href="/components">Components</Breadcrumb.Link>
+        </Breadcrumb.Item>
+      </Breadcrumb.List>
+    </Breadcrumb>
+  ),
+};
