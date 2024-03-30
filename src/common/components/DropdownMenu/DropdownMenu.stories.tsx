1b945be803ab109ba37a5499ff276bec59d4b45d
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-03-31 04:41:53 +0800

    chore: dropdown menu storybook

---
+++ src/common/components/DropdownMenu/DropdownMenu.stories.tsx
@@ -0,0 +1,104 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { useState } from "react";
+import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu";
+
+import { DropdownMenu } from "./DropdownMenu";
+import { Button } from "../Button";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Common/DropdownMenu",
+  component: DropdownMenu,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  args: {},
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+} satisfies Meta<typeof DropdownMenu>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+type Checked = DropdownMenuCheckboxItemProps["checked"];
+
+export const Default: Story = {
+  render: () => (
+    <DropdownMenu>
+      <DropdownMenu.Trigger asChild>
+        <Button variant="primary">Open</Button>
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Content>
+        <DropdownMenu.Label>My Account</DropdownMenu.Label>
+        <DropdownMenu.Separator />
+        <DropdownMenu.Item>Profile</DropdownMenu.Item>
+        <DropdownMenu.Item>Billing</DropdownMenu.Item>
+        <DropdownMenu.Item>Team</DropdownMenu.Item>
+        <DropdownMenu.Item>Subscription</DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu>
+  ),
+};
+
+export const Checkboxes: Story = {
+  render: () => {
+    const [showStatusBar, setShowStatusBar] = useState<Checked>(true);
+    const [showActivityBar, setShowActivityBar] = useState<Checked>(false);
+    const [showPanel, setShowPanel] = useState<Checked>(false);
+
+    return (
+      <DropdownMenu>
+        <DropdownMenu.Trigger asChild>
+          <Button variant="primary">Open</Button>
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content className="w-56">
+          <DropdownMenu.Label>Appearance</DropdownMenu.Label>
+          <DropdownMenu.Separator />
+          <DropdownMenu.CheckboxItem
+            checked={showStatusBar}
+            onCheckedChange={setShowStatusBar}
+          >
+            Status Bar
+          </DropdownMenu.CheckboxItem>
+          <DropdownMenu.CheckboxItem
+            checked={showActivityBar}
+            onCheckedChange={setShowActivityBar}
+            disabled
+          >
+            Activity Bar
+          </DropdownMenu.CheckboxItem>
+          <DropdownMenu.CheckboxItem
+            checked={showPanel}
+            onCheckedChange={setShowPanel}
+          >
+            Panel
+          </DropdownMenu.CheckboxItem>
+        </DropdownMenu.Content>
+      </DropdownMenu>
+    );
+  },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const RadioGroup: Story = {
+  render: () => {
+    const [position, setPosition] = useState("bottom");
+
+    return (
+      <DropdownMenu>
+        <DropdownMenu.Trigger asChild>
+          <Button variant="primary">Open</Button>
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content className="w-56">
+          <DropdownMenu.Label>Panel Position</DropdownMenu.Label>
+          <DropdownMenu.Separator />
+          <DropdownMenu.RadioGroup value={position} onValueChange={setPosition}>
+            <DropdownMenu.RadioItem value="top">Top</DropdownMenu.RadioItem>
+            <DropdownMenu.RadioItem value="bottom">
+              Bottom
+            </DropdownMenu.RadioItem>
+            <DropdownMenu.RadioItem value="right">Right</DropdownMenu.RadioItem>
+          </DropdownMenu.RadioGroup>
+        </DropdownMenu.Content>
+      </DropdownMenu>
+    );
+  },
+};
