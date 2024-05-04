1d41fb00d134622ee92ea343c39375dc0ae78bb5
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-05-05 03:38:42 +0800

    feat: cta card storybook

---
+++ src/common/components/CtaCard/CtaCard.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { CtaCard } from "./CtaCard";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Common/CtaCard",
+  component: CtaCard,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  args: {
+    ctaText: "Write a review",
+    href: "",
+  },
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+} satisfies Meta<typeof CtaCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const Secondary: Story = {
+  ...Default,
+  args: {
+    ...Default.args,
+    variant: "secondary",
+    ctaText: "Contribute to AfterClass OSS",
+  },
+};
