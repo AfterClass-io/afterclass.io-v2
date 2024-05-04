be8dd552ce56d3b2ad2437d27aba866b25e7a013
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-05-04 23:28:20 +0800

    feat: review section to have loading states

--- src/common/components/ReviewSection/ReviewSection.tsx
+++ src/common/components/ReviewSection/ReviewSection.tsx
@@ -1,31 +1,46 @@
+"use client";
+
 import { PenIcon } from "@/common/components/CustomIcon";
 import { ReviewItem } from "@/common/components/ReviewItem";
-import { reviewSectionTheme } from "./index";
-import { type Review } from "@/common/types";
+import { api } from "@/common/tools/trpc/react";
+import { reviewSectionTheme } from "./ReviewSection.theme";
+import { useSession } from "next-auth/react";
+import { ComponentPropsWithoutRef, useEffect } from "react";
+import { ReviewItemSkeleton } from "@/common/components/ReviewItem/ReviewItemSkeleton";
 
-export const ReviewSection = ({
-  isLocked,
-  reviews,
-}: {
-  isLocked: boolean;
-  reviews: Review[];
-}) => {
+export type ReviewSectionProps = ComponentPropsWithoutRef<"div"> & {};
+
+export const ReviewSection = ({ className, ...props }: ReviewSectionProps) => {
   const { wrapper, header, title, icon, reviewsHeader } = reviewSectionTheme();
+  const { status } = useSession();
+  const isUserAuthenticated = status === "authenticated";
+  const { data: reviews, isLoading } = isUserAuthenticated
+    ? api.reviews.getAll.useQuery({})
+    : api.reviews.getAllPublic.useQuery({});
+
+  useEffect(() => {
+    console.log(isLoading || !reviews);
+  }, [isLoading]);
 
   return (
-    reviews && (
-      <div className={wrapper()}>
-        <div className={header()}>
-          <div className={title()}>
-            <PenIcon className={icon()} size={24} />
-            <div className={reviewsHeader()}>Reviews</div>
-          </div>
+    <div className={wrapper({ className })} {...props}>
+      <div className={header()}>
+        <div className={title()}>
+          <PenIcon className={icon()} size={24} />
+          <div className={reviewsHeader()}>Reviews</div>
         </div>
-
-        {reviews.map((review) => (
-          <ReviewItem review={review} key={review.id} isLocked={isLocked} />
-        ))}
       </div>
-    )
+      <>
+        {isLoading || !reviews
+          ? [...Array(10)].map(() => <ReviewItemSkeleton />)
+          : reviews.map((review) => (
+              <ReviewItem
+                review={review}
+                key={review.id}
+                isLocked={!isUserAuthenticated}
+              />
+            ))}
+      </>
+    </div>
   );
 };
