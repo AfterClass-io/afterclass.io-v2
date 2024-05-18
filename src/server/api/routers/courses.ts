c3c79409dc253034c8b0d71374152fefd6a95129
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-05-18 18:42:32 +0800

    feat: add course router query by prof slug
    (cherry picked from commit bec6b0f7b42702800db01fbafaaacb771bd57a46)

--- src/server/api/routers/courses.ts
+++ src/server/api/routers/courses.ts
@@ -33,77 +33,104 @@ const PUBLIC_COURSE_FIELDS = {
 } satisfies Prisma.CoursesSelect;
 
 export const coursesRouter = createTRPCRouter({
-  listByUni: publicProcedure
+  getByUni: publicProcedure
     .input(
       z.object({
         page: z.number().default(1),
         universityId: z.number(),
       }),
     )
-    .query(async ({ ctx, input }) => {
-      const courses = await ctx.db.courses.findMany({
-        skip: DEFAULT_PAGE_SIZE * (input.page - 1),
-        take: DEFAULT_PAGE_SIZE,
-        select: PUBLIC_COURSE_FIELDS,
-        where: {
-          belongToUniversityId: input.universityId,
-        },
-      });
-      return courses;
-    }),
+    .query(
+      async ({ ctx, input }) =>
+        await ctx.db.courses.findMany({
+          skip: DEFAULT_PAGE_SIZE * (input.page - 1),
+          take: DEFAULT_PAGE_SIZE,
+          select: PUBLIC_COURSE_FIELDS,
+          where: {
+            belongToUniversityId: input.universityId,
+          },
+        }),
+    ),
 
-  listByFaculty: publicProcedure
+  getByFaculty: publicProcedure
     .input(
       z.object({
         page: z.number().default(1),
         facultyId: z.number(),
       }),
     )
-    .query(async ({ ctx, input }) => {
-      const courses = await ctx.db.courses.findMany({
-        skip: DEFAULT_PAGE_SIZE * (input.page - 1),
-        take: DEFAULT_PAGE_SIZE,
-        select: PUBLIC_COURSE_FIELDS,
-        where: {
-          belongToFacultyId: input.facultyId,
-        },
-      });
-      return courses;
-    }),
+    .query(
+      async ({ ctx, input }) =>
+        await ctx.db.courses.findMany({
+          skip: DEFAULT_PAGE_SIZE * (input.page - 1),
+          take: DEFAULT_PAGE_SIZE,
+          select: PUBLIC_COURSE_FIELDS,
+          where: {
+            belongToFacultyId: input.facultyId,
+          },
+        }),
+    ),
+  getByProfSlug: publicProcedure
+    .input(
+      z.object({
+        page: z.number().default(1),
+        slug: z.string(),
+      }),
+    )
+    .query(
+      async ({ ctx, input }) =>
+        await ctx.db.courses.findMany({
+          skip: DEFAULT_PAGE_SIZE * (input.page - 1),
+          take: DEFAULT_PAGE_SIZE,
+          select: PUBLIC_COURSE_FIELDS,
+          // see https://www.prisma.io/docs/orm/reference/prisma-client-reference#relation-filters
+          // this query can be read as:
+          // "get all course where some classes have professor with the given slug"
+          where: {
+            classes: {
+              some: {
+                professor: {
+                  slug: input.slug,
+                },
+              },
+            },
+          },
+        }),
+    ),
 
-  listByUniProtected: protectedProcedure
+  getByUniProtected: protectedProcedure
     .input(
       z.object({
         page: z.number().default(1),
         universityId: z.number(),
       }),
     )
-    .query(async ({ ctx, input }) => {
-      const courses = await ctx.db.courses.findMany({
-        skip: DEFAULT_PAGE_SIZE * (input.page - 1),
-        take: DEFAULT_PAGE_SIZE,
-        where: {
-          belongToUniversityId: input.universityId,
-        },
-      });
-      return courses;
-    }),
+    .query(
+      async ({ ctx, input }) =>
+        await ctx.db.courses.findMany({
+          skip: DEFAULT_PAGE_SIZE * (input.page - 1),
+          take: DEFAULT_PAGE_SIZE,
+          where: {
+            belongToUniversityId: input.universityId,
+          },
+        }),
+    ),
 
-  listByFacultyProtected: protectedProcedure
+  getByFacultyProtected: protectedProcedure
     .input(
       z.object({
         page: z.number().default(1),
         facultyId: z.number(),
       }),
     )
-    .query(async ({ ctx, input }) => {
-      const courses = await ctx.db.courses.findMany({
-        skip: DEFAULT_PAGE_SIZE * (input.page - 1),
-        take: DEFAULT_PAGE_SIZE,
-        where: {
-          belongToFacultyId: input.facultyId,
-        },
-      });
-      return courses;
-    }),
+    .query(
+      async ({ ctx, input }) =>
+        await ctx.db.courses.findMany({
+          skip: DEFAULT_PAGE_SIZE * (input.page - 1),
+          take: DEFAULT_PAGE_SIZE,
+          where: {
+            belongToFacultyId: input.facultyId,
+          },
+        }),
+    ),
 });
