aa17b9a219d9c9b5bef08309a784cdcdc479a0a3
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-05-24 03:26:05 +0800

    refactor: remove trailing slash, use startsWith, add inline comments

--- src/env.mjs
+++ src/env.mjs
@@ -37,12 +37,13 @@ export const env = createEnv({
       .transform((str) => {
         let url =
           str ??
-          process.env.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+          // Automatically set by Vercel as system environment variable
+          // NEXT_PUBLIC_VERCEL_URL doesn't include `https`
+          // https://vercel.com/docs/projects/environment-variables/system-environment-variables
+          process.env.NEXT_PUBLIC_VERCEL_URL ??
           "http://localhost:3000/";
-        // Make sure to include `https://` when not localhost.
-        url = url.includes("http") ? url : `https://${url}`;
-        // Make sure to include a trailing `/`.
-        url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+        // Make sure to include `https://` when using NEXT_PUBLIC_VERCEL_URL
+        url = url.startsWith("http") ? url : `https://${url}`;
         return url;
       })
       .pipe(z.string().url()),
