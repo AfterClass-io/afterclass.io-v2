a655841f6ee72fd976cce817d20554c4712213eb
David Lin <13061926+davidlhw@users.noreply.github.com>
2024-05-26 22:23:57 +0800

    feat(ui): home page review section implementation (#137)
    * feat: home review section
    
    * refactor: remove unused `isLoading` prop on review item

--- src/app/(school)/(reviews)/page.tsx
+++ src/app/(school)/(reviews)/page.tsx
@@ -1,9 +1,20 @@
+import { ReviewItem } from "@/common/components/ReviewItem";
 import { ReviewSection } from "@/common/components/ReviewSection";
+import { api } from "@/common/tools/trpc/server";
+import { getServerAuthSession } from "@/server/auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerAuthSession();
+
+  const reviews = session
+    ? await api.reviews.getAllProtected({})
+    : await api.reviews.getAll({});
+
   return (
     <ReviewSection>
-      <div>review section</div>
+      {reviews.map((review) => (
+        <ReviewItem review={review} key={review.id} isLocked={!session} />
+      ))}
     </ReviewSection>
   );
 }
