68a4c9a5b143f62ae11301c716d14e4e8df05da6
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-05-26 22:16:11 +0800

    refactor: remove unused import from seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { Courses, Labels, PrismaClient, ReviewLabelName } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
