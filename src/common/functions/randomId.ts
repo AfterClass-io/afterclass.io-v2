d1aabfc330926206291604dcc6161aaba79abc55
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-05-26 23:23:35 +0800

    refactor: extract magic chars as variable

--- src/common/functions/randomId.ts
+++ src/common/functions/randomId.ts
@@ -1,6 +1,7 @@
 import { customAlphabet } from "nanoid";
 
-const nanoid = customAlphabet("cdefhjkmnprtvwxy2345689", 12);
+const nonAmbiguous = "cdefhjkmnprtvwxy2345689";
+const nanoid = customAlphabet(nonAmbiguous, 12);
 
 export default function randomId() {
   return nanoid(); // returns something like 'cy5p69dym4tx'
