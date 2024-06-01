3d69392690a972f7b40a7627d5873c88356a3269
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-06-01 18:39:58 +0800

    fix: remove searchcmdk

--- src/common/components/CoreLayout/CoreLayout.tsx
+++ src/common/components/CoreLayout/CoreLayout.tsx
@@ -1,23 +1,19 @@
 import { type PropsWithChildren } from "react";
 import { Sidebar } from "../Sidebar";
 import { MobileHeader } from "@/common/components/MobileHeader";
-import { SearchCmdk } from "@/common/components/SearchCmdk/SearchCmdk";
 
 interface CoreLayoutProps extends PropsWithChildren {}
 
 export const CoreLayout = ({ children }: CoreLayoutProps) => {
   return (
-    <>
-      <SearchCmdk />
-      <div className="relative flex h-full flex-col">
-        <MobileHeader className="flex-shrink-0 sm:hidden" />
-        <div className="relative flex h-full flex-col overflow-hidden sm:flex-row">
-          <aside className="relative hidden overflow-y-auto border-r border-border-default bg-surface-base sm:block">
-            <Sidebar />
-          </aside>
-          <main className="relative h-full flex-1 bg-bg-base">{children}</main>
-        </div>
+    <div className="relative flex h-full flex-col">
+      <MobileHeader className="flex-shrink-0 sm:hidden" />
+      <div className="relative flex h-full flex-col overflow-hidden sm:flex-row">
+        <aside className="relative hidden overflow-y-auto border-r border-border-default bg-surface-base sm:block">
+          <Sidebar />
+        </aside>
+        <main className="relative h-full flex-1 bg-bg-base">{children}</main>
       </div>
-    </>
+    </div>
   );
 };
