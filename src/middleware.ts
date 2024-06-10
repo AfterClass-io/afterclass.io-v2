bf171adac4ea0ef6f1886d5e42169305cc570d56
David Lin <13061926+davidlhw@users.noreply.github.com>
2024-06-10 23:08:35 +0800

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,20 +2,7 @@ export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: [
-    /**
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - account/auth (routes used for auth)
-     * - not-found (404 page)
-     * - professor (professor page)
-     * - course (course page)
-     * - search (search page)
-     *
-     * @see https://nextjs.org/docs/app/building-your-application/routing/middleware#matching-paths
-     */
+    // path listed requires authentication
     "/submit",
   ],
 };
