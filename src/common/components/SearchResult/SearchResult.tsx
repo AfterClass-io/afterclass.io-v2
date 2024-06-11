5a198098e8617af2560faa3d2339393c91ca9df0
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-06-12 00:46:30 +0800

    refactor: extract search result divider, empty, list, title subcomponents

--- src/common/components/SearchResult/SearchResult.tsx
+++ src/common/components/SearchResult/SearchResult.tsx
@@ -1,23 +1,12 @@
 import { type ReactNode } from "react";
 import { SearchResultTitle } from "./SearchResultTitle";
+import { SearchResultContent } from "./SearchResultContent";
+import { SearchResultList } from "./SearchResultList";
+import { SearchResultEmpty } from "./SearchResultEmpty";
 import { SearchResultItem } from "./SearchResultItem";
+import { SearchResultDivider } from "./SearchResultDivider";
 import { SearchResultFilter } from "./SearchResultFilter";
 import { searchResultTheme } from "./SearchResult.theme";
-
-const SearchResultContent = ({ children }: { children: ReactNode }) => {
-  const { content } = searchResultTheme();
-  return <div className={content()}>{children}</div>;
-};
-
-const SearchResultList = ({ children }: { children: ReactNode }) => {
-  const { list } = searchResultTheme();
-  return <div className={list()}>{children}</div>;
-};
-
-const SearchResultDivider = () => {
-  const { divider } = searchResultTheme();
-  return <div className={divider()} />;
-};
 
 export const SearchResult = ({ children }: { children: ReactNode }) => {
   const { root } = searchResultTheme();
@@ -28,5 +17,6 @@ SearchResult.Title = SearchResultTitle;
 SearchResult.Content = SearchResultContent;
 SearchResult.List = SearchResultList;
 SearchResult.Item = SearchResultItem;
+SearchResult.Empty = SearchResultEmpty;
 SearchResult.Divider = SearchResultDivider;
 SearchResult.Filter = SearchResultFilter;
