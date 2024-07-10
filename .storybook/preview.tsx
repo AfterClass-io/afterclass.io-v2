f310609bbad840eaf920afe9c44f1c246b065c86
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-07-11 01:44:57 +0800

    fix: storybook complain useSession outside of next-auth session provider

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,6 +2,9 @@ import type { Preview, StoryFn } from "@storybook/react";
 import { withThemeByClassName } from "@storybook/addon-themes";
 import "../src/common/styles/globals.scss";
 import { inter, poppins } from "../src/common/fonts";
+import AuthProvider from "../src/common/providers/AuthProvider";
+import { SessionContext } from "next-auth/react";
+import { mockAuthStates } from "./auth";
 
 const preview: Preview = {
   parameters: {
@@ -20,28 +23,37 @@ const preview: Preview = {
     },
   },
   decorators: [
-    (Story: StoryFn) => (
-      <>
-        {/* @ts-ignore */}
-        <style global jsx>{`
-          :root {
-            --font-inter: ${inter.style.fontFamily};
-            --font-poppins: ${poppins.style.fontFamily};
-          }
-        `}</style>
-        <div
-          style={{
-            padding: "3rem",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-          className={`bg-bg-base`}
-        >
-          <Story />
-        </div>
-      </>
-    ),
+    (Story: StoryFn, { parameters }) => {
+      let mockSession = mockAuthStates.user.session;
+      if (parameters?.mockSession) {
+        mockSession = parameters.mockSession;
+      }
+      return (
+        <AuthProvider>
+          {/* @ts-ignore */}
+          <SessionContext.Provider value={mockSession}>
+            {/* @ts-ignore */}
+            <style global jsx>{`
+              :root {
+                --font-inter: ${inter.style.fontFamily};
+                --font-poppins: ${poppins.style.fontFamily};
+              }
+            `}</style>
+            <div
+              style={{
+                padding: "3rem",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              className={`bg-bg-base`}
+            >
+              <Story />
+            </div>
+          </SessionContext.Provider>
+        </AuthProvider>
+      );
+    },
     withThemeByClassName({
       themes: {
         light: "light",
