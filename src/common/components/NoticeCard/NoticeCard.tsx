ab4489a96eb90a1fd780c14d7331601213cde17f
David <96529759+whoisdavidd@users.noreply.github.com>
2024-07-31 23:40:08 +0800

    fix: coming soon page (#197)
    * feat: coming soon scaffold
    
    * feat: added instruction messages
    
    * feat: Arrows inside Coming Soon box
    
    from the figma some of the pixels are not accurate so I have to change them.
    
    This might be due to the
    1. Box size change (I might accidentally change it)
    
    * feat: Arrows inside Coming Soon box
    
    Fix the import issue and insert the icon inside the custom icon
    
    * update page.tsx
    
    * Update page.tsx
    
    1. Updated the tailwind css. Instead of using css, use the tailwind classes
    2. Deleted the afterclasscomingsoonicon.tsx and use the afterclassicon component
    3. Deleted the package-lock.json
    
    * fix: Coming-soon page
    
    1. Fix the box based on the figma
    2. Figma size and actual size not the same because when i follow the figma size not what i want
    3. Ignore the user modal
    4. Dark mode and light mode working (need help on how to import from the theme component)
    5. Am i parameterize it correctly?
    
    * fix: coming soon page
    
    - Fixed everything
    - Followed the figma
    - Make the notice cards components
    - Updated Storybook to 8.2.6
    
    * Fix: Coming-soon page
    
    - Fixed everything
    - Followed the figma
    - Make the notice cards components
    - Updated Storybook to 8.2.6
    - removed alert and stuff
    
    * fix: Coming-soon page
    
    Fix all the issues!!!
    
    * fix: CS page
    
    i just want to see the build works
    
    ---------
    
    Co-authored-by: KeithLoh <keithloh99@gmail.com>

---
+++ src/common/components/NoticeCard/NoticeCard.tsx
@@ -0,0 +1,37 @@
+import { NoticeCardTheme,NoticeCardVariants } from "./NoticeCard.theme";
+import { AfterclassIcon } from "@/common/components/CustomIcon";
+
+
+export type NoticeCardProps = NoticeCardVariants & {
+  title: string;
+  subtitle: string;
+};
+export const NoticeCard = ({title, subtitle}: NoticeCardProps) => {
+  const{
+    wrapper,
+    textBox,
+    icon,
+    icons,
+    title: titleClassNames,
+    subtitle: subtitleClassNames,
+  } = NoticeCardTheme();
+  
+  return (
+    <div className={wrapper()}>
+      <div className={icons()}>
+      <AfterclassIcon className={icon({ size: "xl" })} />
+      <AfterclassIcon className={icon({ size: "lg" })} />
+      <AfterclassIcon className={icon({ size: "md" })} />
+        <div className={textBox()}>
+          <div className={titleClassNames()}>
+            {title}
+          </div>
+          <div className={subtitleClassNames()}>
+            {subtitle}
+          </div>
+        </div>
+      </div>
+    </div>
+  
+  );
+};
