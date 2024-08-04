6b8a035993bba2deecb64b5f04e693f6de4a6eb0
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-08-04 23:23:32 +0800

    fix: build failure

--- src/app/account/auth/layout.tsx
+++ src/app/account/auth/layout.tsx
@@ -1,6 +1,6 @@
 import { type PropsWithChildren } from "react";
 
-export default async function SchoolLayout({ children }: PropsWithChildren) {
+export default function AuthLayout({ children }: PropsWithChildren) {
   return (
     <div className="overflow-y-scroll">
       <section className="flex flex-shrink-0 items-start justify-center p-5 md:items-center md:py-16">
