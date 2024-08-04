90bb2b4de294e4b4e2c4433ba612298437e24d21
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-08-04 23:07:47 +0800

    fix: revert remove suspense boundary on login form

--- src/app/account/auth/login/page.tsx
+++ src/app/account/auth/login/page.tsx
@@ -1,10 +1,13 @@
 import { AuthCard, LoginForm } from "@/common/components/Auth";
+import { Suspense } from "react";
 
 export default function Login() {
   return (
     <section className="flex h-full flex-shrink-0 items-start justify-center p-5 md:items-center md:py-16">
       <AuthCard title="Login">
-        <LoginForm />
+        <Suspense fallback={<div>Loading...</div>}>
+          <LoginForm />
+        </Suspense>
       </AuthCard>
     </section>
   );
