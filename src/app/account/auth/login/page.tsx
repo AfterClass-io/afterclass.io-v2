7186c0b53923106dd038f79355a144eacdc974c6
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-08-04 22:35:02 +0800

    chore: make auth card responsive

--- src/app/account/auth/login/page.tsx
+++ src/app/account/auth/login/page.tsx
@@ -4,11 +4,9 @@ import { Suspense } from "react";
 export default function Login() {
   return (
     <>
-      <section className="flex h-full flex-shrink-0 items-center justify-center py-16">
+      <section className="flex h-full flex-shrink-0 items-start justify-center p-5 md:items-center md:py-16">
         <AuthCard title="Login">
-          <Suspense fallback={<div>Loading...</div>}>
-            <LoginForm />
-          </Suspense>
+          <LoginForm />
         </AuthCard>
       </section>
     </>
