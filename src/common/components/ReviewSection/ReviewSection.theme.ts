e13e05b68dc8a350301d41b92236eb410118a667
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-08-13 21:12:31 +0800

    Revert "refactor: make review section fit to review items instead of fill container"
    This reverts commit be197cb4e1d4b223e79232acce231171602e964c.

--- src/common/components/ReviewSection/ReviewSection.theme.ts
+++ src/common/components/ReviewSection/ReviewSection.theme.ts
@@ -4,7 +4,7 @@ export type ReviewSectionVariants = VariantProps<typeof reviewSectionTheme>;
 export const reviewSectionTheme = tv(
   {
     slots: {
-      wrapper: ["flex", "flex-col", "bg-surface-base", "rounded-3xl", "w-fit"],
+      wrapper: ["flex", "flex-col", "bg-surface-base", "rounded-3xl", "w-full"],
       header: ["flex", "items-center", "justify-between"],
       title: ["flex", "items-center", "gap-4"],
       icon: ["h-6", "w-6", "rotate-90"],
