5026275617601efb6bdf8e86eb0ad067d18300d7
David Lin <13061926+davidlhw@users.noreply.github.com>
2024-08-16 00:04:39 +0800

    feat(ui): review item responsive mobile viewport  (#210)
    * chore: abstract profiles as independent component
    
    * refactor: group header by reviewer and reviewee groups
    
    * refactor: move style classes to theme file
    
    * refactor: move review modal under review item component
    
    * refactor: remove redundant div on profile component
    
    * refactor: use revieweegroup on reviewmodal
    
    * refactor: make review section fit to review items instead of fill container
    
    * Merge branch 'main' into 204-mobile-review-item
    
    * refactor: text size on review modal
    
    * refactor: use size variant instead of breakpoint prefixes
    
    * Revert "refactor: make review section fit to review items instead of fill container"
    
    This reverts commit be197cb4e1d4b223e79232acce231171602e964c.

---
+++ src/common/components/Profile/Profile.theme.ts
@@ -0,0 +1,13 @@
+import { type VariantProps, tv } from "tailwind-variants";
+
+export type ProfileVariants = VariantProps<typeof profileTheme>;
+
+export const profileTheme = tv(
+  {
+    slots: {
+      wrapper: ["flex", "items-center", "gap-2"],
+      name: ["overflow-hidden", "text-sm", "text-ellipsis", "text-text-em-mid"],
+    },
+  },
+  { responsiveVariants: true },
+);
