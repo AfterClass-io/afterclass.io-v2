cd3b1df1a117c1e40e4dd334906ea7c4318dbd99
Alvin Ling <66348533+AlvinLingg@users.noreply.github.com>
2024-08-24 11:21:42 +0800

    chore: allow passing icons as props to ctacard (#224)

--- src/common/components/CtaCard/CtaCard.tsx
+++ src/common/components/CtaCard/CtaCard.tsx
@@ -1,9 +1,15 @@
 import {
+  type ReactNode,
+  type ReactElement,
+  isValidElement,
+  cloneElement,
+} from "react";
+
+import {
   Button,
   type ButtonVariants,
   type ButtonLinkOrAnchorProps,
 } from "@/common/components/Button";
-import { EditIcon, PlusIcon } from "@/common/components/CustomIcon";
 
 import { type CtaCardVariants, ctaCardTheme } from "./CtaCard.theme";
 
@@ -11,17 +17,31 @@ export type CtaCardProps = CtaCardVariants &
   ButtonVariants &
   ButtonLinkOrAnchorProps & {
     ctaText: string;
+    leftIcon?: ReactNode;
+    rightIcon?: ReactNode;
   };
 
-export const CtaCard = ({ ctaText, ...props }: CtaCardProps) => {
+export const CtaCard = ({
+  ctaText,
+  leftIcon,
+  rightIcon,
+  ...props
+}: CtaCardProps) => {
   const { button, ctaWrapper, cta, icon } = ctaCardTheme();
+  const renderIcon = (iconElement: ReactNode) => {
+    if (!isValidElement(iconElement)) {
+      return null;
+    }
+    return cloneElement(iconElement as ReactElement, { className: icon() });
+  };
+
   return (
     <Button as="a" className={button()} asChild {...props}>
       <div className={ctaWrapper()}>
-        <PlusIcon className={icon()} />
+        {renderIcon(leftIcon)}
         <span className={cta()}>{ctaText}</span>
       </div>
-      <EditIcon className={icon()} />
+      {renderIcon(rightIcon)}
     </Button>
   );
 };
