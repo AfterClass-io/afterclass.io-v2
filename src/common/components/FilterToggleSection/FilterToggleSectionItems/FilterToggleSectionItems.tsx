86d9f624e0487ec890e5a3071060cd4bc66d05a3
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-08-27 01:24:52 +0800

    chore: expose component api for filter item wrapper `items` subcomponent

--- src/common/components/FilterToggleSection/FilterToggleSectionItems/FilterToggleSectionItems.tsx
+++ src/common/components/FilterToggleSection/FilterToggleSectionItems/FilterToggleSectionItems.tsx
@@ -1,16 +1,22 @@
-import { type ReactNode } from "react";
+import { type ReactNode, ComponentPropsWithoutRef } from "react";
 import { FilterToggleSectionItemsSkeleton } from "../FilterToggleSectionSkeleton";
 import { filterToggleSectionTheme } from "../FilterToggleSection.theme";
 
 export const FilterToggleSectionItems = ({
   children,
-}: {
+  ...props
+}: ComponentPropsWithoutRef<"div"> & {
   children: ReactNode;
 }) => {
+  const { className, ...rest } = props;
   const { container } = filterToggleSectionTheme({
     size: { initial: "sm", md: "md" },
   });
-  return <div className={container()}>{children}</div>;
+  return (
+    <div className={container({ className })} {...rest}>
+      {children}
+    </div>
+  );
 };
 
 FilterToggleSectionItems.Skeleton = FilterToggleSectionItemsSkeleton;
