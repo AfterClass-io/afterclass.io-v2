f33445f11c77464358588b1246cdbd47a75c974d
Alvin Ling <66348533+AlvinLingg@users.noreply.github.com>
2024-08-28 00:46:29 +0800

    fix: mobile view login button (#230)
    conditionally render login / write review depending on login status of user

--- src/common/components/CoreLayout/CoreLayout.tsx
+++ src/common/components/CoreLayout/CoreLayout.tsx
@@ -1,13 +1,18 @@
 import { type PropsWithChildren } from "react";
 import { Sidebar } from "../Sidebar";
 import { MobileHeader } from "@/common/components/MobileHeader";
+import { getServerAuthSession } from "@/server/auth";
 
 interface CoreLayoutProps extends PropsWithChildren {}
 
-export const CoreLayout = ({ children }: CoreLayoutProps) => {
+export async function CoreLayout({ children }: CoreLayoutProps) {
+  const session = await getServerAuthSession();
   return (
     <div className="flex h-dvh flex-col">
-      <MobileHeader className="flex-shrink-0 sm:hidden" />
+      <MobileHeader
+        className="flex-shrink-0 sm:hidden"
+        isLoggedIn={!!session}
+      />
       <div className="flex flex-1 overflow-hidden">
         <aside className="relative hidden shrink-0 overflow-y-auto border-r border-border-default bg-surface-base sm:block">
           <Sidebar />
@@ -18,4 +23,4 @@ export const CoreLayout = ({ children }: CoreLayoutProps) => {
       </div>
     </div>
   );
-};
+}
