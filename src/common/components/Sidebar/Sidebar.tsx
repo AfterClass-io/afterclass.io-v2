5dc4e8bcff00892736e53c36f65f70adfaf1df26
Jaylin0312 <jielinglee01@gmail.com>
2024-08-29 02:26:03 +0800

    feat: new sidebar items

--- src/common/components/Sidebar/Sidebar.tsx
+++ src/common/components/Sidebar/Sidebar.tsx
@@ -3,15 +3,19 @@
 import React, { type ComponentPropsWithoutRef } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import * as Separator from "@radix-ui/react-separator";
 
 import {
   ChartLineIcon,
   StarLineAltIcon,
+  TelegramIcon,
+  HelpDeskIcon,
 } from "@/common/components/CustomIcon";
 import { SidebarItem } from "@/common/components/SidebarItem";
 import { Logo } from "@/common/components/Logo";
 import { SearchCmdk } from "@/common/components/SearchCmdk";
 import { cn } from "@/common/functions";
+import { env } from "@/env.mjs";
 
 const SIDEBAR_ITEMS = [
   {
@@ -19,16 +23,26 @@ const SIDEBAR_ITEMS = [
     icon: <StarLineAltIcon size={16} />,
     href: "/",
     exact: true,
+    category: "feature",
   },
   {
     label: "Bid History",
     icon: <ChartLineIcon />,
     href: "/bidding",
+    category: "feature",
   },
-
-
-  
-
+  {
+    label: "Telegram",
+    icon: <TelegramIcon size={16} />,
+    href: env.NEXT_PUBLIC_AC_TELEGRAM_LINK,
+    category: "channel",
+  },
+  {
+    label: "Helpdesk",
+    icon: <HelpDeskIcon size={16} />,
+    href: env.NEXT_PUBLIC_AC_HELPDESK_LINK,
+    category: "channel",
+  },
   // Development-only links
   ...(process.env.NODE_ENV === "development" ? [] : []),
 ];
@@ -46,7 +60,12 @@ export const Sidebar = ({
   ...props
 }: SidebarProps) => {
   const pathname = usePathname();
-
+  const SIDEBAR_FEATURES_ITEMS = SIDEBAR_ITEMS.filter(
+    (item) => item.category === "feature",
+  );
+  const SIDEBAR_CHANNELS_ITEMS = SIDEBAR_ITEMS.filter(
+    (item) => item.category === "channel",
+  );
   return (
     <div
       {...props}
@@ -60,7 +79,7 @@ export const Sidebar = ({
       {!hideSearch && <SearchCmdk />}
       <div>
         <ul className="space-y-2">
-          {SIDEBAR_ITEMS.map((item) => (
+          {SIDEBAR_FEATURES_ITEMS.map((item) => (
             <SidebarItem
               key={item.href}
               {...item}
@@ -70,6 +89,18 @@ export const Sidebar = ({
                   : pathname?.startsWith(item.href) // pathname is null in storybook context
               }
             />
+          ))}
+        </ul>
+      </div>
+      <Separator.Root
+        className="h-px w-full bg-border-default"
+        decorative
+        orientation="horizontal"
+      />
+      <div>
+        <ul className="space-y-2">
+          {SIDEBAR_CHANNELS_ITEMS.map((item) => (
+            <SidebarItem key={item.href} {...item} external={true} />
           ))}
         </ul>
       </div>
