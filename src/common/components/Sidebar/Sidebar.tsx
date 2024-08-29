ac01e8afa5a8ca5753a0da0bb11e2e9e844f8464
Jaylin0312 <jielinglee01@gmail.com>
2024-08-29 23:16:33 +0800

    feat: add submit review button and link to github on side menu on mobile view

--- src/common/components/Sidebar/Sidebar.tsx
+++ src/common/components/Sidebar/Sidebar.tsx
@@ -10,6 +10,8 @@ import {
   StarLineAltIcon,
   TelegramIcon,
   HelpDeskIcon,
+  PlusIcon,
+  GithubIcon,
 } from "@/common/components/CustomIcon";
 import { SidebarItem } from "@/common/components/SidebarItem";
 import { Logo } from "@/common/components/Logo";
@@ -30,6 +32,18 @@ const SIDEBAR_ITEMS = [
     icon: <ChartLineIcon />,
     href: "/bidding",
     category: "feature",
+  },
+  {
+    label: "Write a Review",
+    icon: <PlusIcon size={16} />,
+    href: "/submit",
+    category: "contribute",
+  },
+  {
+    label: "AfterClass OSS",
+    icon: <GithubIcon size={16} />,
+    href: "https://github.com/AfterClass-io/afterclass.io-v2",
+    category: "contribute",
   },
   {
     label: "Channel",
@@ -66,6 +80,9 @@ export const Sidebar = ({
   const SIDEBAR_CHANNELS_ITEMS = SIDEBAR_ITEMS.filter(
     (item) => item.category === "channel",
   );
+  const SIDEBAR_CONTRIBUTE_ITEMS = SIDEBAR_ITEMS.filter(
+    (item) => item.category === "contribute",
+  );
   return (
     <div
       {...props}
@@ -97,7 +114,27 @@ export const Sidebar = ({
         decorative
         orientation="horizontal"
       />
+      {/* Contribute section, only visible on mobile */}
+      <div className="block md:hidden">
+        <div className="px-3 py-2 text-xs text-text-em-low">Contribute</div>
+        <ul className="space-y-2">
+          {SIDEBAR_CONTRIBUTE_ITEMS.map((item) => (
+            <SidebarItem
+              key={item.href}
+              {...item}
+              active={pathname === item.href}
+              external={item.href.startsWith("http") ? true : false}
+            />
+          ))}
+        </ul>
+      </div>
+      <Separator.Root
+        className="block h-px w-full bg-border-default md:hidden"
+        decorative
+        orientation="horizontal"
+      />
       <div>
+        <div className="px-3 py-2 text-xs text-text-em-low">Telegram</div>
         <ul className="space-y-2">
           {SIDEBAR_CHANNELS_ITEMS.map((item) => (
             <SidebarItem key={item.href} {...item} external={true} />
