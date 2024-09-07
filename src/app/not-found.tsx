cffcaa2a55c22611f33e3877d7139875d2e394ca
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-09-07 12:37:06 +0800

    feat(ui): update 404 with notice card

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,30 @@
-import Link from "next/link";
+import { Button } from "@/common/components/Button";
+import { NoticeCard } from "@/common/components/NoticeCard";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="flex justify-center p-6 md:h-full md:items-center md:p-12">
+      <NoticeCard title="Not Found" isError>
+        <Button
+          as="a"
+          href="/"
+          variant="link"
+          className="inline text-[length:inherit]"
+        >
+          Click here to return to Home.
+        </Button>
+        <span className="inline">Otherwise, you can get help from us</span>
+        <Button
+          as="a"
+          href="/"
+          variant="link"
+          className="inline px-1 text-[length:inherit]"
+          external
+        >
+          @afterclass
+        </Button>
+        <span className="inline">on Telegram.</span>
+      </NoticeCard>
     </div>
   );
 }
