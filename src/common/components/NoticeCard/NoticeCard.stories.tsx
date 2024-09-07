a27d5895c06efc2ba1763c24c3ff81ac1e4e6e80
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-09-07 12:24:36 +0800

    feat(ui): noticecard accept reactnode as children

--- src/common/components/NoticeCard/NoticeCard.stories.tsx
+++ src/common/components/NoticeCard/NoticeCard.stories.tsx
@@ -1,24 +1,32 @@
 import type { Meta, StoryObj } from "@storybook/react";
+
 import { NoticeCard } from "./NoticeCard";
 
-
-const meta: Meta<typeof NoticeCard> = {
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
   title: "Common/NoticeCard",
   component: NoticeCard,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
-  args: {
-    title: "Custom Icons Title",
-    subtitle: "Custom Icons Subtitle",
-  },
-};
+  args: {},
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+} satisfies Meta<typeof NoticeCard>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-
 export const Default: Story = {
   args: {
     title: "Coming Soon",
-    subtitle: "Watch out for the red dot in the sidebar once this is done!",
+    children: "Watch out for the red dot in the sidebar once this is done!",
+  },
+};
+
+export const Error: Story = {
+  args: {
+    title: "Oh no!",
+    children:
+      "Click here to retry. Otherwise, you can get help from us @afterclass on Telegram.",
+    isError: true,
   },
 };
--- src/common/components/NoticeCard/NoticeCard.tsx
+++ src/common/components/NoticeCard/NoticeCard.tsx
@@ -1,37 +1,36 @@
-import { NoticeCardTheme,NoticeCardVariants } from "./NoticeCard.theme";
 import { AfterclassIcon } from "@/common/components/CustomIcon";
-
+import { noticeCardTheme, NoticeCardVariants } from "./NoticeCard.theme";
+import { type ReactNode } from "react";
 
 export type NoticeCardProps = NoticeCardVariants & {
   title: string;
-  subtitle: string;
+  children: ReactNode;
 };
-export const NoticeCard = ({title, subtitle}: NoticeCardProps) => {
-  const{
+
+export const NoticeCard = ({ title, children, isError }: NoticeCardProps) => {
+  const {
     wrapper,
-    textBox,
+    floatingIcons,
     icon,
-    icons,
+    textContainer,
     title: titleClassNames,
-    subtitle: subtitleClassNames,
-  } = NoticeCardTheme();
-  
+    children: childrenClassNames,
+  } = noticeCardTheme({
+    size: { initial: "sm", md: "md" },
+  });
   return (
     <div className={wrapper()}>
-      <div className={icons()}>
-      <AfterclassIcon className={icon({ size: "xl" })} />
-      <AfterclassIcon className={icon({ size: "lg" })} />
-      <AfterclassIcon className={icon({ size: "md" })} />
-        <div className={textBox()}>
-          <div className={titleClassNames()}>
-            {title}
-          </div>
-          <div className={subtitleClassNames()}>
-            {subtitle}
-          </div>
-        </div>
+      <div className={floatingIcons({ isError })}>
+        <AfterclassIcon className={icon({ iconSize: "xl" })} />
+        <AfterclassIcon className={icon({ iconSize: "lg" })} />
+        <AfterclassIcon className={icon({ iconSize: "md" })} />
+        <AfterclassIcon className={icon({ iconSize: "sm" })} />
+      </div>
+
+      <div className={textContainer()}>
+        <div className={titleClassNames()}>{title}</div>
+        <div className={childrenClassNames()}>{children}</div>
       </div>
     </div>
-  
   );
 };
