600f97a49e19a5f865acfb35144abcf9fffa54e7
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-09-10 18:17:06 +0800

    feat(ui): add global error as fallback

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,4 +1,9 @@
-"use client";
+"use client"; // Error boundaries must be Client Components
+import { useEffect } from "react";
+
+import { Button } from "@/common/components/Button";
+import { NoticeCard } from "@/common/components/NoticeCard";
+import { env } from "@/env.mjs";
 
 export default function GlobalError({
   error,
@@ -7,12 +12,35 @@ export default function GlobalError({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
   return (
     <html>
       <body>
-        <h2>Something went wrong!</h2>
-        <h3>{error.message}</h3>
-        <button onClick={() => reset()}>Try again</button>
+        <div className="flex justify-center p-6 md:h-full md:items-center md:p-12">
+          <NoticeCard title="Something went wrong!" isError>
+            <Button
+              variant="link"
+              className="inline text-[length:inherit]"
+              onClick={() => reset()}
+            >
+              Click here to try again.
+            </Button>
+            <span className="inline">Otherwise, you can get help from us</span>
+            <Button
+              as="a"
+              href={env.NEXT_PUBLIC_AC_HELPDESK_LINK}
+              variant="link"
+              className="inline px-1 text-[length:inherit]"
+              external
+            >
+              @afterclass
+            </Button>
+            <span className="inline">on Telegram.</span>
+          </NoticeCard>
+        </div>
       </body>
     </html>
   );
