7312850fdda487bdca1bb1d722f5131b872e72e5
David Lin <13061926+davidlhw@users.noreply.github.com>
2024-09-16 01:57:06 +0800

    feat(ui): implement noticecard component on coming soon and error pages (#236)
    * feat(ui): noticecard accept reactnode as children
    
    * chore: simplify school layout padding
    
    * feat(ui): update 404 with notice card
    
    * feat(ui): coming soon notice card for bidding
    
    * feat(ui): remove unused pages
    
    * feat(ui): rename bid sidebar item
    
    * style: minor css changes to noticecard
    
    * feat(ui): add global error as fallback
    
    * style: add font and theme to global error
    
    * feat(ui): add root error boundary handler for pages inside rootlayout

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,4 +1,11 @@
-"use client";
+"use client"; // Error boundaries must be Client Components
+import { useEffect } from "react";
+
+import { inter, poppins } from "@/common/fonts";
+import { Button } from "@/common/components/Button";
+import { NoticeCard } from "@/common/components/NoticeCard";
+import { env } from "@/env.mjs";
+import ThemeProvider from "@/common/providers/ThemeProvider";
 
 export default function GlobalError({
   error,
@@ -7,12 +14,39 @@ export default function GlobalError({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
   return (
-    <html>
+    <html lang="en" className={`${inter.variable} ${poppins.variable}`}>
       <body>
-        <h2>Something went wrong!</h2>
-        <h3>{error.message}</h3>
-        <button onClick={() => reset()}>Try again</button>
+        <ThemeProvider>
+          <div className="flex justify-center p-6 md:h-full md:items-center md:p-12">
+            <NoticeCard title="Something went wrong!" isError>
+              <Button
+                variant="link"
+                className="inline text-[length:inherit]"
+                onClick={() => reset()}
+              >
+                Click here to try again.
+              </Button>
+              <span className="inline">
+                Otherwise, you can get help from us
+              </span>
+              <Button
+                as="a"
+                href={env.NEXT_PUBLIC_AC_HELPDESK_LINK}
+                variant="link"
+                className="inline px-1 text-[length:inherit]"
+                external
+              >
+                @afterclass
+              </Button>
+              <span className="inline">on Telegram.</span>
+            </NoticeCard>
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );
