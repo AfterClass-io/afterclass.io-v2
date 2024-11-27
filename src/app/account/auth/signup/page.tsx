c3600653ca493dbcd9bd264258de51a53d41f209
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-11-28 03:47:11 +0800

    feat: modal explaining to v1 user on pwd reset procedure

--- src/app/account/auth/signup/page.tsx
+++ src/app/account/auth/signup/page.tsx
@@ -1,9 +1,22 @@
-import { AuthCard, SignupForm } from "@/modules/auth/components";
+import {
+  AuthCard,
+  SignupForm,
+  SignupModalV1User,
+} from "@/modules/auth/components";
 
-export default function SignUp() {
+export default function SignUp({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const existingV1UserEmail = searchParams.email;
+
   return (
-    <AuthCard title="Create an account">
-      <SignupForm />
-    </AuthCard>
+    <>
+      {existingV1UserEmail && <SignupModalV1User />}
+      <AuthCard title="Create an account">
+        <SignupForm />
+      </AuthCard>
+    </>
   );
 }
--- src/modules/auth/components/index.ts
+++ src/modules/auth/components/index.ts
@@ -1,6 +1,7 @@
 export * from "./AuthCard";
 export * from "./LoginForm";
 export * from "./SignupForm";
+export * from "./SignupModalV1User";
 export * from "./ResetPasswordForm";
 export * from "./ConfirmSignUpNote";
 export * from "./ForgotPwdForm";
