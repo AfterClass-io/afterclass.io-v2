53fe267ee1ce5c6e5bb575b690d74beb4997d899
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-12-06 02:26:35 +0800

    Revert "test: cypress ci failture - remove localstorage calls"
    This reverts commit 18a36f5f37f5e22321d9ae0eefd2755856b47374.

--- src/modules/home/components/AnnouncementBanner/AnnouncementBanner.tsx
+++ src/modules/home/components/AnnouncementBanner/AnnouncementBanner.tsx
@@ -1,54 +1,54 @@
 "use client";
-// import { useEffect, useState } from "react";
-// import { useAtom } from "jotai";
-// import { atomWithStorage } from "jotai/utils";
+import { useEffect, useState } from "react";
+import { useAtom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
 
 import { XCloseIcon } from "@/common/components/CustomIcon";
 import { Button } from "@/common/components/Button";
 import { env } from "@/env";
-// import { useEdgeConfigs } from "@/common/providers/EdgeConfigProvider";
+import { useEdgeConfigs } from "@/common/providers/EdgeConfigProvider";
 
-// const hasCloseRevertOldUiAtom = atomWithStorage("hasCloseRevertOldUi", false);
+const hasCloseRevertOldUiAtom = atomWithStorage("hasCloseRevertOldUi", false);
 
 export const AnnouncementsBanner = () => {
-  // const [isShown, setIsShown] = useState(false);
-  // const [hasClosed, setHasClosed] = useAtom(hasCloseRevertOldUiAtom);
-  // const edgeConfig = useEdgeConfigs();
+  const [isShown, setIsShown] = useState(false);
+  const [hasClosed, setHasClosed] = useAtom(hasCloseRevertOldUiAtom);
+  const edgeConfig = useEdgeConfigs();
 
-  // useEffect(() => {
-  //   setIsShown(!hasClosed && edgeConfig.test);
-  // }, [edgeConfig, hasClosed]);
+  useEffect(() => {
+    setIsShown(!hasClosed && edgeConfig.test);
+  }, [edgeConfig, hasClosed]);
 
-  // const handleClose = () => {
-  //   setIsShown(false);
-  //   setHasClosed(true);
-  // };
+  const handleClose = () => {
+    setIsShown(false);
+    setHasClosed(true);
+  };
 
   return (
-    // isShown && (
-    <div className="relative flex w-full items-center justify-center gap-6 bg-primary-dark p-2 text-xs md:gap-2 md:p-1 md:text-sm">
-      <span className="flex flex-col gap-1 text-text-on-primary md:flex-row">
-        <span>We have a new look!</span>
-        <span>Missed the old AfterClass?</span>
-      </span>
-      <Button
-        as="a"
-        variant="link"
-        className="inline-flex h-fit p-0 pb-[1px] font-bold text-text-on-primary underline hover:text-secondary-default md:h-fit md:p-0 md:text-sm"
-        href={env.NEXT_PUBLIC_OLD_SITE_URL}
-        external
-        isResponsive
-      >
-        Bring me back
-      </Button>
-      <Button
-        variant="ghost"
-        className="absolute right-0 inline font-bold text-text-on-primary hover:bg-transparent hover:after:bg-transparent"
-        iconLeft={<XCloseIcon className="h-4 w-4" />}
-        aria-label="close"
-        // onClick={handleClose}
-      />
-    </div>
-    // )
+    isShown && (
+      <div className="relative flex w-full items-center justify-center gap-6 bg-primary-dark p-2 text-xs md:gap-2 md:p-1 md:text-sm">
+        <span className="flex flex-col gap-1 text-text-on-primary md:flex-row">
+          <span>We have a new look!</span>
+          <span>Missed the old AfterClass?</span>
+        </span>
+        <Button
+          as="a"
+          variant="link"
+          className="inline-flex h-fit p-0 pb-[1px] font-bold text-text-on-primary underline hover:text-secondary-default md:h-fit md:p-0 md:text-sm"
+          href={env.NEXT_PUBLIC_OLD_SITE_URL}
+          external
+          isResponsive
+        >
+          Bring me back
+        </Button>
+        <Button
+          variant="ghost"
+          className="absolute right-0 inline font-bold text-text-on-primary hover:bg-transparent hover:after:bg-transparent"
+          iconLeft={<XCloseIcon className="h-4 w-4" />}
+          aria-label="close"
+          onClick={handleClose}
+        />
+      </div>
+    )
   );
 };
