7d3171d85866d628e0eff3126844d26dfe709cf3
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-12-16 20:30:45 +0800

    chore: bump trpc structure and deps see t3app

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,5 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
+export default {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
   },
 };
-
-export default config;
