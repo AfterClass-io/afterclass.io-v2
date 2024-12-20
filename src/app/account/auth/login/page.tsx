083d1b348ef97283630e8bf9e16c6fa06ee05dd7
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-12-21 07:22:24 +0800

    fix: missing suspense with csr bailout

--- src/app/account/auth/login/page.tsx
+++ src/app/account/auth/login/page.tsx
@@ -1,9 +1,17 @@
+import { Suspense } from "react";
+
 import { AuthCard, LoginForm } from "@/modules/auth/components";
 
 export default function Login() {
   return (
     <AuthCard title="Login">
-      <LoginForm />
+      {/*
+          //! suspense required for useSearchParams
+          see https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout
+      */}
+      <Suspense fallback={<div>Loading...</div>}>
+        <LoginForm />
+      </Suspense>
     </AuthCard>
   );
 }
